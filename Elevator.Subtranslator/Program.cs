a4017200b57b27a0ce427b13c2af5b156ba900a3
Andrey Lemin <andrey.lemin@dataart.com>
2017-10-05 02:56:51 +0300

    Add some filters

--- Elevator.Subtranslator/Program.cs
+++ Elevator.Subtranslator/Program.cs
@@ -7,6 +7,7 @@ using CommandLine;
 using CommandLine.Text;
 using System.IO;
 using System.Xml.Linq;
+using System.Globalization;
 
 namespace Elevator.Subtranslator
 {
@@ -48,8 +49,12 @@ namespace Elevator.Subtranslator
 			Options options = parseResult.Value;
 
 			XDocument mergedDoc = MergeDefs(options.DefsLocation);
+			mergedDoc.Save(Path.Combine(options.OutputLocation, "MergedDefs.xml"));
 
-			mergedDoc.Save(Path.Combine(options.OutputLocation, "Defs.xml"));
+			string[] excludeElementSuffixes = new string[] { "depth", "width", "height", "class", "texture", "category", "type", "path", "tags", "techLevel", "timeOfDay", "mode" };
+
+			XDocument filteredDoc = FilterStringDefs(mergedDoc, excludeElementSuffixes);
+			filteredDoc.Save(Path.Combine(options.OutputLocation, "FilteredDefs.xml"));
 		}
 
 		static XDocument MergeDefs(string defsFullPath)
@@ -69,6 +74,128 @@ namespace Elevator.Subtranslator
 				}
 			}
 			return mergedXml;
+		}
+
+		static XDocument FilterStringDefs(XDocument doc, string[] excludeElementSuffixes)
+		{
+			XDocument filteredDoc = new XDocument();
+			XElement filteredDefs = new XElement("Defs");
+			filteredDoc.Add(filteredDefs);
+
+			HashSet<string> defNames = GetDefNames(doc);
+
+			foreach (XElement def in doc.Root.Elements())
+			{
+				XElement filteredDef = FilterStringValues(def, defNames, excludeElementSuffixes);
+				if (filteredDef != null)
+				{
+					filteredDefs.Add(filteredDef);
+				}
+			}
+
+			return filteredDoc;
+		}
+
+		static HashSet<string> GetDefNames(XDocument doc)
+		{
+			XElement defs = doc.Root;
+
+			HashSet<string> defNames = new HashSet<string>();
+
+			foreach (XElement def in defs.Elements())
+			{
+				XElement defNameElem = def.Element("defName");
+				if (defNameElem != null)
+				{
+					defNames.Add(defNameElem.Value);
+				}
+				else
+				{
+					XAttribute defNameAttrib = def.Attribute("Name");
+					if (defNameAttrib != null)
+					{
+						defNames.Add(defNameAttrib.Value);
+					}
+				}
+			}
+
+			defNames.Distinct();
+
+			return defNames;
+		}
+
+		static XElement FilterStringValues(XElement element, HashSet<string> defNames, string[] excludeSuffixes)
+		{
+			if (element == null)
+				return null;
+
+			if (HasOneOfSuffixes(element.Name.LocalName, excludeSuffixes))
+				return null;
+
+			XElement filteredElement = new XElement(element.Name);
+
+			if (element.HasElements)
+			{
+				foreach (XElement child in element.Elements())
+				{
+					XElement filteredChild = FilterStringValues(child, defNames, excludeSuffixes);
+					if (filteredChild != null)
+					{
+						filteredElement.Add(filteredChild);
+					}
+				}
+
+				return filteredElement.HasElements ? filteredElement : null;
+			}
+
+			if (!string.IsNullOrEmpty(element.Value))
+			{
+				if (SeemsHavingStringValue(element, defNames, excludeSuffixes))
+				{
+					filteredElement.Value = element.Value;
+					return filteredElement;
+				}
+
+				return null;
+			}
+
+			return null;
+		}
+
+		static bool SeemsHavingStringValue(XElement element, HashSet<string> defNames, string[] excludeSuffixes)
+		{
+			if (HasOneOfSuffixes(element.Name.LocalName, excludeSuffixes)) return false;
+
+			string value = element.Value;
+
+			bool boolValue;
+			if (bool.TryParse(value, out boolValue)) return false;
+
+			int intValue;
+			if (int.TryParse(value, out intValue)) return false;
+
+			float floatValue;
+			if (float.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out floatValue)) return false;
+
+			if (value.StartsWith("(") && value.EndsWith(")"))
+				return false;
+
+			if (value.StartsWith("RGBA"))
+				return false;
+
+			if (defNames.Contains(value)) return false;
+
+			return true;
+		}
+
+		static bool HasOneOfSuffixes(string value, string[] suffixes)
+		{
+			foreach (string suffix in suffixes)
+			{
+				if (value.EndsWith(suffix, true, CultureInfo.InvariantCulture))
+					return true;
+			}
+			return false;
 		}
 	}
 }
