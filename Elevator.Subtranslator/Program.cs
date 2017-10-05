015c821e36e3f8d77c6e378b5b005058348d43c9
Andrey Lemin <andrey.lemin@dataart.com>
2017-10-06 00:55:13 +0300

    Add localization diffing

--- Elevator.Subtranslator/Program.cs
+++ Elevator.Subtranslator/Program.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommandLine;
-using CommandLine.Text;
 using System.IO;
 using System.Xml.Linq;
-using System.Globalization;
 
 namespace Elevator.Subtranslator
 {
 	class Options
 	{
 		[Option('d', "defs", Required = true, HelpText = "Definition folder location.")]
-		public string DefsLocation { get; set; }
+		public string DefsPath { get; set; }
 
 		[Option('i', "injections", Required = true, HelpText = "Localized 'DefInjected' folder location.")]
-		public string InjectionsLocation { get; set; }
+		public string InjectionsPath { get; set; }
+
+		[Option('e', "etalon", Required = true, HelpText = "Etalon 'DefInjected' folder location.")]
+		public string InjectionsEtalonPath { get; set; }
 
 		[Option('o', "output", Required = false, HelpText = "Output folder location.")]
-		public string OutputLocation { get; set; }
+		public string OutputPath { get; set; }
 	}
 
 	class DefDirectoryNameComparer : IEqualityComparer<string>
@@ -48,13 +46,23 @@ namespace Elevator.Subtranslator
 
 			Options options = parseResult.Value;
 
-			XDocument mergedDoc = MergeDefs(options.DefsLocation);
-			mergedDoc.Save(Path.Combine(options.OutputLocation, "MergedDefs.xml"));
+			XDocument mergedInjectionsEtalon = MergeInjections(options.InjectionsEtalonPath);
+			XDocument mergedInjections = MergeInjections(options.InjectionsPath);
 
-			string[] excludeElementSuffixes = new string[] { "depth", "width", "height", "class", "texture", "category", "type", "path", "tags", "techLevel", "timeOfDay", "mode" };
+			List<string> mergedInjectionsEtalonTags = GetChildrenNames(mergedInjectionsEtalon.Root).ToList();
+			List<string> mergedInjectionsTags = GetChildrenNames(mergedInjections.Root).ToList();
 
-			XDocument filteredDoc = FilterStringDefs(mergedDoc, excludeElementSuffixes);
-			filteredDoc.Save(Path.Combine(options.OutputLocation, "FilteredDefs.xml"));
+			IEnumerable<string> itemsToTranslate = mergedInjectionsEtalonTags.Except(mergedInjectionsTags);
+			IEnumerable<string> itemsToDelete = mergedInjectionsTags.Except(mergedInjectionsEtalonTags);
+
+			File.WriteAllLines(Path.Combine(options.OutputPath, "ItemsToTranslate.txt"), itemsToTranslate);
+			File.WriteAllLines(Path.Combine(options.OutputPath, "ItemsToDelete.txt"), itemsToDelete);
+
+			//XDocument mergedDoc = MergeDefs(options.DefsPath);
+			//string[] excludeElementSuffixes = new string[] { "depth", "width", "height", "class", "texture", "category", "type", "path", "tags", "techLevel", "timeOfDay", "mode" };
+
+			//XDocument filteredDoc = FilterStringDefs(mergedDoc, excludeElementSuffixes);
+			//filteredDoc.Save(Path.Combine(options.OutputPath, "FilteredDefs.xml"));
 		}
 
 		static XDocument MergeDefs(string defsFullPath)
@@ -76,24 +84,31 @@ namespace Elevator.Subtranslator
 			return mergedXml;
 		}
 
-		static XDocument FilterStringDefs(XDocument doc, string[] excludeElementSuffixes)
+		static XDocument MergeInjections(string injectionsFullPath)
 		{
-			XDocument filteredDoc = new XDocument();
-			XElement filteredDefs = new XElement("Defs");
-			filteredDoc.Add(filteredDefs);
+			XDocument mergedXml = new XDocument();
+			XElement mergedDefs = new XElement("LanguageData");
+			mergedXml.Add(mergedDefs);
 
-			HashSet<string> defNames = GetDefNames(doc);
-
-			foreach (XElement def in doc.Root.Elements())
+			foreach (string injectionsFilePath in Directory.EnumerateFiles(injectionsFullPath, "*.xml", SearchOption.AllDirectories))
 			{
-				XElement filteredDef = FilterStringValues(def, defNames, excludeElementSuffixes);
-				if (filteredDef != null)
+				XDocument injectionsDoc = XDocument.Load(injectionsFilePath);
+				XElement languageData = injectionsDoc.Root;
+
+				foreach (XElement injection in languageData.Elements())
 				{
-					filteredDefs.Add(filteredDef);
+					mergedDefs.Add(new XElement(injection));
 				}
 			}
+			return mergedXml;
+		}
 
-			return filteredDoc;
+		static IEnumerable<string> GetChildrenNames(XElement element)
+		{
+			foreach (XElement child in element.Elements())
+			{
+				yield return child.Name.LocalName;
+			}
 		}
 
 		static HashSet<string> GetDefNames(XDocument doc)
@@ -122,80 +137,6 @@ namespace Elevator.Subtranslator
 			defNames.Distinct();
 
 			return defNames;
-		}
-
-		static XElement FilterStringValues(XElement element, HashSet<string> defNames, string[] excludeSuffixes)
-		{
-			if (element == null)
-				return null;
-
-			if (HasOneOfSuffixes(element.Name.LocalName, excludeSuffixes))
-				return null;
-
-			XElement filteredElement = new XElement(element.Name);
-
-			if (element.HasElements)
-			{
-				foreach (XElement child in element.Elements())
-				{
-					XElement filteredChild = FilterStringValues(child, defNames, excludeSuffixes);
-					if (filteredChild != null)
-					{
-						filteredElement.Add(filteredChild);
-					}
-				}
-
-				return filteredElement.HasElements ? filteredElement : null;
-			}
-
-			if (!string.IsNullOrEmpty(element.Value))
-			{
-				if (SeemsHavingStringValue(element, defNames, excludeSuffixes))
-				{
-					filteredElement.Value = element.Value;
-					return filteredElement;
-				}
-
-				return null;
-			}
-
-			return null;
-		}
-
-		static bool SeemsHavingStringValue(XElement element, HashSet<string> defNames, string[] excludeSuffixes)
-		{
-			if (HasOneOfSuffixes(element.Name.LocalName, excludeSuffixes)) return false;
-
-			string value = element.Value;
-
-			bool boolValue;
-			if (bool.TryParse(value, out boolValue)) return false;
-
-			int intValue;
-			if (int.TryParse(value, out intValue)) return false;
-
-			float floatValue;
-			if (float.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out floatValue)) return false;
-
-			if (value.StartsWith("(") && value.EndsWith(")"))
-				return false;
-
-			if (value.StartsWith("RGBA"))
-				return false;
-
-			if (defNames.Contains(value)) return false;
-
-			return true;
-		}
-
-		static bool HasOneOfSuffixes(string value, string[] suffixes)
-		{
-			foreach (string suffix in suffixes)
-			{
-				if (value.EndsWith(suffix, true, CultureInfo.InvariantCulture))
-					return true;
-			}
-			return false;
 		}
 	}
 }
